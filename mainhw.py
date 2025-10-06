fe6b49999ded97eff8e1c2eef915a7559a8c2cef
Swyft06 <160307112+Swyft06@users.noreply.github.com>
2025-10-05 19:54:32 -0700

    Create mainhw.py

---
+++ mainhw.py
@@ -0,0 +1,22 @@
+from tkinter import *
+root = Tk()
+root.geometry("200x150")
+root.title("Gui")
+
+label = Label(root,text="")
+label.pack()
+
+scrollbar = Scrollbar(root)
+scrollbar.pack(side=RIGHT,fill=Y)
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
