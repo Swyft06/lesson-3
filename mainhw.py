253e92139748c58daa4f64b80f53b7d071b065ff
Swyft06 <160307112+Swyft06@users.noreply.github.com>
2025-10-12 19:05:07 -0700

    Update mainhw.py

--- mainhw.py
+++ mainhw.py
@@ -1,7 +1,7 @@
 from tkinter import *
 root = Tk()
-root.geometry("200x150")
-root.title("Gui")
+root.geometry("300x150")
+root.title("Shopping list")
 
 label = Label(root,text="")
 label.pack()
@@ -9,13 +9,22 @@ label.pack()
 scrollbar = Scrollbar(root)
 scrollbar.pack(side=RIGHT,fill=Y)
 
+listbox = Listbox(root,yscrollcommand=scrollbar.set)
 
+listbox.insert(1,"Lettuce")
+listbox.insert(2,"Tomato")
+listbox.insert(3,"Banana")
+listbox.insert(4,"Milk")
+listbox.insert(5,"Eggs")
+listbox.insert(6,"Potato")
+listbox.insert(7,"Onion")
+listbox.insert(8,"Pasta")
+listbox.insert(9,"Apples")
+listbox.insert(10,"Orange")
 
+listbox.pack()
 
-
-
-
-
+scrollbar.config(command=listbox.yview)
 
 
 
